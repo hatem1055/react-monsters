cacecd69e44678a26b23dc7ac2c5128a6ed1b812
hatem1055 <hatemmostafa31@gmail.com>
2020-09-11 16:38:54 -0700

    Updates

---
+++ precache-manifest.9f5964c9aec4c79d623d9b4357a5b018.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88a4a1bd71c9dc05ed45febcd0f2b4b2",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "40078b82cd4b6d675789",
+    "url": "/react-monsters/static/css/main.46eeae64.chunk.css"
+  },
+  {
+    "revision": "dec1cef2a37e9ca4deb4",
+    "url": "/react-monsters/static/js/2.e9dc3c13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters/static/js/2.e9dc3c13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40078b82cd4b6d675789",
+    "url": "/react-monsters/static/js/main.0b2e653e.chunk.js"
+  },
+  {
+    "revision": "90fbba813a35afb5d768",
+    "url": "/react-monsters/static/js/runtime-main.1ac8d056.js"
+  }
+]);
